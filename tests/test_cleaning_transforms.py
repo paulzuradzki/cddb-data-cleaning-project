03674c05764246afafad23239901cabedbbac2c1
Paul Zuradzki <paulzuradzki@gmail.com>
2023-07-28 21:26:12 -0500

    unit tests for cleaning functions

---
+++ tests/test_cleaning_transforms.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pytest
+from clean_cddb.cleaning_transforms import (
+    clean_value_standardize_various_artist,
+    clean_value_try_to_fix_encoding_errors,
+)
+
+
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    [
+        # Test cases for clean_value_standardize_various_artist() function
+        ("John Doe", "John Doe"),
+        ("Various Artists", "Various"),
+        ("Various Artist", "Various"),
+        ("various", "Various"),
+        ("VaRiOuS aRtIsT", "Various"),
+        ("Various", "Various"),
+        ("", ""),
+        (123, "123"),
+        ("   Led Zeppelin   ", "Led Zeppelin"),
+    ],
+)
+def test_clean_value_standardize_various_artist(input_value, expected_output):
+    assert clean_value_standardize_various_artist(input_value) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_value, expected_output",
+    # Test cases to test that bad encodings are getting fixed
+    [
+        ("NguyÃªn LÃª", "Nguyên Lê"),
+        ("Olle LjungstrÃ¶m", "Olle Ljungström"),
+        ("Camerata FlorianÃ³polis", "Camerata Florianópolis"),
+        ("LeÃ¦ther Strip", "Leæther Strip"),
+        ("BÃ¼rger Lars Dietrich", "Bürger Lars Dietrich"),
+        ("MÃ¶gel", "Mögel"),
+    ],
+)
+def test_clean_value_try_to_fix_encoding_errors(input_value, expected_output):
+    assert clean_value_try_to_fix_encoding_errors(input_value) == expected_output
